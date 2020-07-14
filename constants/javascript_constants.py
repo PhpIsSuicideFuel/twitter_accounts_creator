78ebdcdf25ee58e0cb5b3a27f9b4980af7947be7
PhpIsSuicideFuel <55277384+PhpIsSuicideFuel@users.noreply.github.com>
2020-07-14 14:55:51 +0300

    Update javascript_constants.py

--- constants/javascript_constants.py
+++ constants/javascript_constants.py
@@ -8,10 +8,8 @@ SCRIPTS = [
     """,
     """
     (() => {
-    // We can mock this in as much depth as we need for the test.
     window.navigator.chrome = {
     runtime: {},
-    // etc.
     };
     })()
     """,
@@ -27,17 +25,13 @@ SCRIPTS = [
     """,
     """
     (() => {
-    // Overwrite the `plugins` property to use a custom getter.
     Object.defineProperty(navigator, 'plugins', {
-    // This just needs to have `length > 0` for the current test,
-    // but we could mock the plugins too if necessary.
     get: () => [1, 2, 3, 4, 5],
     });
     })()
     """,
     """
     (() => {
-    // Overwrite the `plugins` property to use a custom getter.
     Object.defineProperty(navigator, 'languages', {
     get: () => ['en-US', 'en'],
     });
